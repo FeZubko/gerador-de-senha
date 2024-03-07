2c8653d2831b08248f0c38e81cf9363bfbd348f5
Felipe Zubko <92220978+FeZubko@users.noreply.github.com>
2024-03-07 16:47:01 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Image, Slider, StyleSheet, Button, Alert } from "react-native";
 
-export default function App() {
+function App() {
+  const [tamanhoSenha, setTamanhoSenha] = useState(8);
+  const geraSenha = () => {
+    alert("Senha gerada com sucesso");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Styles.box}>
+      <Image source={require('./assets/icone-cadeado.png')} style={Styles.logo}/>
+      <Text style={Styles.title}>{tamanhoSenha} caracteres </Text>
+      <Button title="GERAR SENHA" onPress={geraSenha}></Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const Styles = StyleSheet.create({
+  box: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#274135', // Cor de fundo verde escuro
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginVertical: 20,
+    color: '#ffffff', // Cor do texto branca
   },
 });
+
+export default App;
