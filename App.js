399f9e8bdf8cded40494536890ea1b79055f35c4
Felipe Zubko <92220978+FeZubko@users.noreply.github.com>
2024-03-07 21:49:35 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
-import React, { useState } from "react";
-import { View, Text, Image, Slider, StyleSheet, Button, Alert } from "react-native";
+import { StyleSheet, Text, View } from "react-native-web";
+import Tela1 from "./src/componentes/tela1";
+import Tela2 from "./src/componentes/tela2";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  const [tamanhoSenha, setTamanhoSenha] = useState(8);
-  const geraSenha = () => {
-    alert("Senha gerada com sucesso");
-  };
 
   return (
-    <View style={Styles.box}>
-      <Image source={require('./assets/icone-cadeado.png')} style={Styles.logo}/>
-      <Text style={Styles.title}>{tamanhoSenha} caracteres </Text>
-      <Button title="GERAR SENHA" onPress={geraSenha}></Button>
-    </View>
-  );
-}
-
-const Styles = StyleSheet.create({
-  box: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#274135', // Cor de fundo verde escuro
-  },
-  logo: {
-    width: 200,
-    height: 200,
-    resizeMode: 'contain',
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginVertical: 20,
-    color: '#ffffff', // Cor do texto branca
-  },
-});
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="NovaTela">
+        <Stack.Screen name="home" component={Tela1}/>
+        <Stack.Screen name="tela2" component={Tela2}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+};
 
 export default App;
